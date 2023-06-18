4ebd7a42338a7eb516b181eb1f9bff29dbe60cc2
VuQuanKa12 <vupvde160363@fpt.edu.vn>
2023-06-18 16:40:55 +0700

    test

---
+++ CRUD_Demo_WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CRUD_Demo_WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
